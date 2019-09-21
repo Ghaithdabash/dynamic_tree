0d22689ef565790def0bec60c591aaa65debe7b1
ghaithdabash <ghaithdabash@gmail.com>
2019-09-21 19:09:27 +0300

    Update redis settings

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,5 @@
-$redis = Redis::Namespace.new("DynamicTree", :redis => Redis.new)
+#$redis = Redis::Namespace.new("DynamicTree", :redis => Redis.new)
+uri = URI.parse(ENV["REDIS_URL"])
+$redis = Redis::Namespace.new(
+            url: ENV["REDIS_URL"],
+          )
